47a7cff0f5354266954370f3d641750e0282c6a2
Wardell <wjames@student.neumont.edu>
2014-07-03 09:43:50 -0600

    Created PrimeIterator.java

---
+++ Lab 1_Steganog/Steganog/lab1/PrimeIterator.java
@@ -0,0 +1,23 @@
+package lab1;
+
+import java.util.Iterator;
+
+public class PrimeIterator implements Iterator<Integer> {
+
+	public PrimeIterator(int max) 
+	{
+		
+	}
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
